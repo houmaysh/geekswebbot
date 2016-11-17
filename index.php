6d70cafa28fc1a608c0a50ad7a2215f214da1120
houmaysh <houmaysh@users.noreply.github.com>
2016-11-18 01:26:38 +0330

    updatedbot

--- index.php
+++ index.php
@@ -1,75 +1,30 @@
 <?php
 
-/*
-* This file is part of GeeksWeb Bot (GWB).
-*
-* GeeksWeb Bot (GWB) is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 3
-* as published by the Free Software Foundation.
-* 
-* GeeksWeb Bot (GWB) is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.  <http://www.gnu.org/licenses/>
-*
-* Author(s):
-*
-* © 2015 Kasra Madadipouya <kasra@madadipouya.com>
-*
-*/
+
 require 'vendor/autoload.php';
 
-$client = new Zelenin\Telegram\Bot\Api(''); // Set your access token
-$url = ''; // URL RSS feed
+$client = new Zelenin\Telegram\Bot\Api('290977281:AAEZtfBKhmalujsj2Vg_F9nE2872ZqNsUVY'); // Set your access token
 $update = json_decode(file_get_contents('php://input'));
+
 
 //your app
 try {
 
-    if($update->message->text == '/email')
+    if($update->message->text == '/نمایش عکس')
     {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-        	'chat_id' => $update->message->chat->id,
-        	'text' => "You can send email to : Kasra@madadipouya.com"
+    	$response = $client->sendPhoto(['chat_id' => $update->message->chat->id, 'AgADBAADBKgxG6audg4pwRAWLPbAob1oZBkABC0nj_53FB4fiMgAAgI']);
+
      	]);
     }
-    else if($update->message->text == '/help')
+    else if($update->message->text == '/تماس')
     {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-    		'chat_id' => $update->message->chat->id,
-    		'text' => "List of commands :\n /email -> Get email address of the owner \n /latest -> Get latest posts of the blog 
-    		/help -> Shows list of available commands"
+    	$response = $client->sendContact(['chat_id' => $update->message->chat->id, '09352000414', 'Hom' , 'Shahriyari']);
+
     		]);
 
     }
-    else if($update->message->text == '/latest')
-    {
-    		Feed::$cacheDir 	= __DIR__ . '/cache';
-			Feed::$cacheExpire 	= '5 hours';
-			$rss 		= Feed::loadRss($url);
-			$items 		= $rss->item;
-			$lastitem 	= $items[0];
-			$lastlink 	= $lastitem->link;
-			$lasttitle 	= $lastitem->title;
-			$message = $lasttitle . " \n ". $lastlink;
-			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-			$response = $client->sendMessage([
-					'chat_id' => $update->message->chat->id,
-					'text' => $message
-				]);
 
-    }
-    else
-    {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-    		'chat_id' => $update->message->chat->id,
-    		'text' => "Invalid command, please use /help to get list of available commands"
-    		]);
-    }
-
+}
 } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
 
     //echo error message ot log it
